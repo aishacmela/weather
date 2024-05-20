2f9b8bb024a8294adf7c1786a14d1aca9d3d83ec
Aishacmela <aishacmela@gmail.com>
2024-05-20 15:56:48 +0200

    moved weather information in the weather info component

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import "./Weather.css";
 import axios from "axios";
-import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo";
 
 export default function Weather(props) {
 
@@ -11,12 +11,14 @@ export default function Weather(props) {
 
   function handleResponse(response){
   setWeatherData({
-    date: new Date(response.data.dt * 1000),  
+    date: new Date(response.data.dt * 1000),
+    city: response.data.city,  
     temperature: response.data.main.temp,
     pressure: response.data.main.pressure,
     wind: response.data.wind.speed,
     humidity: response.data.main.humidity,
     icon: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+    description: response.data.weather[0].description
   });
     setReady(true);
     console.log(response.data); 
@@ -39,39 +41,9 @@ return (
           value="search" 
           className="submit-form col-3" />
       </form>
-
-      <div className="row mt-5">
-        <div className="col-sm-4 text-center">
-          <h2 className="text-capitalize">{props.defaultCity}</h2>
-          <FormattedDate date={weatherData.date} />
-        </div>
-        <div className="col-sm-4 text-center">
-          <img
-            src={weatherData.icon}
-            alt=""
-          />
-        </div>
-
-        <div className="col-sm-4 text-center">
-          <ul>
-            <li>
-              Pressure : <strong>{weatherData.pressure}</strong>
-            </li>
-            <li>
-              Humidity : <strong>{weatherData.humidity}%</strong>
-            </li>
-            <li>
-              Wind : <strong> {weatherData.wind}km/h</strong>
-            </li>
-          </ul>
-          <div className="citytemperature">
-            <span className="temperature">{Math.round(weatherData.temperature)}</span>
-            <span className="unit">°C | F</span>
-          </div>
-        </div>
+      <WeatherInfo data={weatherData}/>
       </div>
-    </div>
-  );
+      );
     } else {
         
     const apiKey = `480d20dfa70092c931000b6330ebfc1e`;
---
+++ src/WeatherInfo.js
@@ -0,0 +1,43 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+
+
+export default function WeatherInfo(props){
+    return(
+       <div>
+        <div className="row mt-5">
+        <div className="col-sm-4 text-center">
+          <h2 className="text-capitalize">{props.data.city}</h2>
+          <FormattedDate date={props.data.date} />
+          <p>{props.data.description}</p>
+        </div>
+        <div className="col-sm-4 text-center">
+          <img
+            src={props.data.icon}
+            alt=""
+          />
+        </div>
+
+        <div className="col-sm-4 text-center">
+          <ul>
+            <li>
+              Pressure : <strong>{props.data.pressure}</strong>
+            </li>
+            <li>
+              Humidity : <strong>{props.data.humidity}%</strong>
+            </li>
+            <li>
+              Wind : <strong> {props.data.wind}km/h</strong>
+            </li>
+          </ul>
+          <div className="citytemperature">
+            <span className="temperature">{Math.round(props.data.temperature)}</span>
+            <span className="unit">°C | F</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  );
+
+}
